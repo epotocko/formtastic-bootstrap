383d3e7882e962ff4903bcd70907b308ffb7d555
Eddie <epotocko@gmail.com>
2015-10-16 16:18:23 -0400

    Overriding the date picker to localize the date using the translations

--- lib/formtastic-bootstrap/inputs/date_picker_input.rb
+++ lib/formtastic-bootstrap/inputs/date_picker_input.rb
@@ -4,6 +4,20 @@ module FormtasticBootstrap
       include Base
       include Base::Stringish
       include Base::DatetimePickerish
+
+      def value
+        return options[:input_html][:value] if options[:input_html] && options[:input_html].key?(:value)
+        val = object.send(method)
+        return val if val.nil?
+        if val.is_a?(Time)
+          val = Date.new(val.year, val.month, val.day)
+        end
+        if val.is_a?(Date)
+          return I18n.localize(val, format: :short)
+        end
+        val.to_s
+      end
+
     end
   end
 end
